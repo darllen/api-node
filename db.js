32c58a66f67c97d0d1204a17ad032496fcbc2763
Anunciada <jamilly.d.castro@accenture.com>
2024-04-04 18:44:11 -0300

    atividade 6

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
+const env = require("dotenv")
+const Sequelize = require("sequelize");
 
-dotenv.config();
+env.config()
 
-
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-    dialect: process.env.DIALECT, 
-    host: process.env.HOST
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER , process.env.DATABASE_PASSWORD , {
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT,
 });
 
 module.exports = sequelize;
